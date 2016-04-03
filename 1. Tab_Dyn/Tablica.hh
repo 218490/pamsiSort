e3febc23f80c598190c4bd3e23ebac48eaf28592
218711 <218711@student.pwr.edu.pl>
2016-04-04 01:19:34 +0200

    Porządkowanie folderów.

---
+++ 1. Tab_Dyn/Tablica.hh
@@ -0,0 +1,22 @@
+#ifndef TABLICA_HH
+#define TABLICA_HH
+// klasa zawierajaca tablice dynamiczna i metody do jej powiekszania/pomniejszania
+class Tablica
+{
+public:
+	Tablica();
+	~Tablica();
+private:
+	int Rozmiar = 10;
+	int * tablica = new int[Rozmiar];
+public:
+
+	void Powiekszanie_O1();				// powiekszanie tablicy o jedno miejsce az do momentu dopasowania
+	void Powiekszanie_xRazy(int);		// powiekszanie tablicy dwukrotnie
+	void Powiekszanie_Potega();			// powiekszanie tablicy potegowo
+	void SetSize(int);					// pomniejszanie tablicy do zadanego wymiaru
+	int	WhatSize();						// Zwraca rozmiar tablicy
+	void FillArray(int,int);			// Uzupelnia tablice w miescu arg1, wartoscia arg2
+	int Print(int);						// zwraca wartoœæ w tablicy o indeksie argumentu
+};
+#endif
---
+++ 1. Tab_Dyn/tablica_dynamiczna.cpp
@@ -0,0 +1,65 @@
+#include "Tablica.hh"
+#include <iostream>
+#include <sys/time.h>
+
+using namespace std;
+
+
+int main()
+{
+	// zmienne potrzebne do pomiaru czasu
+	timeval *start = new timeval;
+	timeval *stop = new timeval;
+	// zmienna okreslajaca iloma elementami "inicjujemy" tablice
+	int Nowy_Rozmiar;
+	// zmienne do testow
+	Tablica Tab_1;
+	Tablica Tab_2;
+	Tablica Tab_3;
+	int PowiekszXRazy = 2;
+
+	cout << "Poczatkowy rozmiar tablicy tablicy:" << Tab_1.WhatSize() << endl;//Tab_1.Rozmiar << endl;
+	cout << "Wprowadz nowy wymiar tablicy: ";
+	cin >> Nowy_Rozmiar;
+
+	cout << "O ile razy chcesz zwiekszac tablice:";
+	cin >> PowiekszXRazy;
+
+	//Zwiekszanie rozmiaru tablicy o 1 miejsce
+    gettimeofday(start, NULL);
+	while (Nowy_Rozmiar > Tab_1.WhatSize())	Tab_1.Powiekszanie_O1();//Tab_1.tablica = Tab_1.Powiekszanie_O1();
+	gettimeofday(stop, NULL);
+	double czas_1 = (stop->tv_sec-start->tv_sec)*1000000 + stop->tv_usec-start->tv_usec;
+
+	//Zwiekszanie rozmiaru tablicy dwukrotnie
+	gettimeofday(start,NULL);
+	while (Nowy_Rozmiar > Tab_2.WhatSize()) Tab_2.Powiekszanie_xRazy(PowiekszXRazy);//Tab_2.tablica = Tab_2.Powiekszanie_xRazy(PowiekszXRazy);
+	if (Nowy_Rozmiar < Tab_2.WhatSize()) Tab_2.SetSize(Nowy_Rozmiar);//Tab_2.tablica = Tab_2.SetSize(Nowy_Rozmiar);
+	gettimeofday(stop, NULL);
+	double czas_2 = (stop->tv_sec-start->tv_sec)*1000000 + stop->tv_usec-start->tv_usec;
+
+
+	//Zwiekszanie rozmiaru tablicy do kwadratu obecnego rozmiaru
+	gettimeofday(start,NULL);
+	while (Nowy_Rozmiar>Tab_3.WhatSize()) Tab_3.Powiekszanie_Potega();//Tab_3.tablica = Tab_3.Powiekszanie_Potega();
+	if (Nowy_Rozmiar < Tab_3.WhatSize()) Tab_3.SetSize(Nowy_Rozmiar);//Tab_3.tablica = Tab_3.SetSize(Nowy_Rozmiar);
+	gettimeofday(stop, NULL);
+	double czas_3 = (stop->tv_sec-start->tv_sec)*1000000 + stop->tv_usec-start->tv_usec;
+
+	cout << "Czas wykonywania algorytmu zwiekszania zakresu tablicy zajmuje:" << endl;
+	cout << "O jedno pole: \t\t\t\t\t" << czas_1 << " mikrosekund" << endl
+		<< "O zadana krotnosc powiekszenia: \t\t" << czas_2 << " mikrosekund" << endl
+		<< "O kwadrat obecnego rozmiaru: \t\t\t" << czas_3 << " mikrosekund" << endl;
+
+/*
+	for (int i = 0; i < Nowy_Rozmiar; i++) Tab_1.FillArray(i, i);
+	for (int i = 0; i < Nowy_Rozmiar; i++) cout << Tab_1.Print(i)<<".";
+
+	Tab_1.Powiekszanie_O1();
+	for (int i = 0; i < Nowy_Rozmiar; i++) cout << Tab_1.Print(i)<<".";
+*/
+
+
+	return 0;
+}
+
